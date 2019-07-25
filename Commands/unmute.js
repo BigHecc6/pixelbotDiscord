4338afbfe48bd67ceac47d848e906af42bdfe20e
TVG28 <thevillagergamer28@gmail.com>
2019-07-24 21:03:01 -0400

    Still trying to fix the same thing lmao

--- Commands/unmute.js
+++ Commands/unmute.js
@@ -11,7 +11,7 @@ module.exports = {
       return message.reply("no one was mentioned.");
     } else if (message.mentions.users.bot) {
       return message.reply("I cannot mute bots. You can do it yourself locally.");
-    } else if (!message.mentions.members.roles.has(role)) {
+    } else if (!message.mentions.members.roles.id(role)) {
       return message.channel.send('This member is not muted.');
     }
     const toMute = message.mentions.users.first();
