beb1d9c2e8ef23b367cb0d75c3be1b06956de540
TVG28 <thevillagergamer28@gmail.com>
2019-07-24 20:48:06 -0400

    Test

--- Commands/unmute.js
+++ Commands/unmute.js
@@ -9,11 +9,7 @@ module.exports = {
       return message.reply("no one was mentioned.");
     } else if (message.mentions.users.bot) {
       return message.reply("I cannot mute bots. You can do it yourself locally.");
-<<<<<<< HEAD
     } else if (!message.mentions.members.roles.has('603715750288556043')) {
-=======
-    } else if (message.mentions.members.roles.has('603715750288556043')) {
->>>>>>> 2d197833c42e1ca13a29d0c8d1770a638e8a8992
       return message.channel.send('This member is not muted.');
     }
     const toMute = message.mentions.users.first();
