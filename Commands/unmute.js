2c4faca10d5e68494c0e30a5ffec593157d8d7f2
TVG28 <thevillagergamer28@gmail.com>
2019-07-24 20:59:57 -0400

    Turned MUTED Role into a variable

--- Commands/unmute.js
+++ Commands/unmute.js
@@ -3,13 +3,15 @@ module.exports = {
   description: 'Unmutes a specified member. Context: !unmute (@member)',
   guildOnly: true,
   execute(message, args) {
+    const role = message.guild.roles.find('id', 'MUTED');
+
     if (!message.member.permissions.has('MUTE_MEMBERS')) {
       return message.reply("you don't have the required permission.");
     } else if (!message.mentions.users.size) {
       return message.reply("no one was mentioned.");
     } else if (message.mentions.users.bot) {
       return message.reply("I cannot mute bots. You can do it yourself locally.");
-    } else if (!message.mentions.members.roles.has(603715750288556043)) {
+    } else if (!message.mentions.members.roles.has(role)) {
       return message.channel.send('This member is not muted.');
     }
     const toMute = message.mentions.users.first();
